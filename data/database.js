1e05af02877881e9734b7309d7fb7e374cdcf496
Eloize <eloize.muzel@aluno.senai.br>
2026-03-03 11:11:42 -0300

    03.03

---
+++ data/database.js
@@ -0,0 +1,29 @@
+let categorias = [
+    { id: 1, nome: 'Medicamentos' },
+    { id: 2, nome: 'Cirurgias' },
+    { id: 3, nome: 'Petiscos' },
+];
+
+let produtos = [
+    {
+        categoriaId: 1,
+        nome: 'Bravecto – Antipulgas e Carrapatos',
+        descricao: "Medicamento parasiticida que protege cães contra pulgas e carrapatos por 3 meses com uma única dose.",
+        preco: 179.90,
+        imagem: 'https://http2.mlstatic.com/D_643289-MLB51992761630_102022-C.jpg'
+    },
+    {
+        categoriaId: 2,
+        nome: 'Castração',
+        descricao: "Pacote Cirúrgico: Avaliação pré‑anestésica, Anestesia geral, Antibióticos e analgésicos pós‑operatórios",
+        preco: 450.00,
+        imagem: 'https://cobasi.vteximg.com.br/arquivos/ids/302772/Roupa-Pos-Cirurgica-para-Caes-Machos-Castracao-Azul-Pet-Med-2.jpg?v=638888051443630000'
+    },
+    {
+        categoriaId: 3,
+        nome: 'Vet Snack Weight Control 85g',
+        descricao: "Petisco de baixa caloria desenhado especialmente para controle de peso, ideal para cães com tendência à obesidade ou em dieta.",
+        preco: 26.90,
+        imagem: 'https://www.happydoguk.com/cdn/shop/files/VET_Snack_HD_Skin_853b8e81-5b46-4757-b4e8-b710e437313f_1080x.jpg?v=1741192407'
+    },
+]
