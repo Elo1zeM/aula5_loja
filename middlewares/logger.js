a0e0477edd24a694cd002df537a7fdf84543e193
Eloize <eloize.muzel@aluno.senai.br>
2026-03-10 13:45:06 -0300

    Delete middlewares directory

--- middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-const loggerMiddleware = (req, res, next) => {
-    const horaAtual = new Date().toLocaleTimeString();
-    console.log(`[${horaAtual}] Requisição recebida:
-        ${req.method} ${req.url}`);
-    next();
-};
-
-module.exports = loggerMiddleware;
